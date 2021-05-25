4bab65e07f047bb3e5c780e5c4a9183f64ffb133
subho26 <subho26@subho26.com>
2021-05-25 16:46:08 +0530

    added service discovery and apigateway in auth service

--- AuthServer/src/main/java/com/auth/config/SecurityConfig.java
+++ AuthServer/src/main/java/com/auth/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().antMatchers("/authenticate")
+        http.csrf().disable().authorizeRequests().antMatchers("/auth/authenticate")
                 .permitAll().anyRequest().authenticated()
                 .and().exceptionHandling().and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
