9eb22f2f96d96d770f649c8985b143594957ff7b
subho <subho@subho.subho>
2022-09-07 11:11:23 +0530

    working config server

--- account-service/src/main/java/com/accountservice/accountservice/MessageRestController.java
+++ account-service/src/main/java/com/accountservice/accountservice/MessageRestController.java
@@ -1,18 +1,19 @@
 package com.accountservice.accountservice;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-public class AccountServiceApplication {
+@RestController
+@RefreshScope
+public class MessageRestController {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AccountServiceApplication.class, args);
-	}
+    @Value("${propertyMessage:Hello world - Config Server is not working..Please check configuration }")
+    private String message;
 
+    @RequestMapping("/getMessage")
+    String getMessage() {
+        return this.message;
+    }
 }
-
